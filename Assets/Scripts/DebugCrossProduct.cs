f2de0829922987c91ec0df47b7cc847206f17231
Ringo <ringo_kam@hotmail.com>
2020-10-25 16:42:20 -0400

    world to local and local to world?

---
+++ Assets/Scripts/DebugCrossProduct.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DebugCrossProduct : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        var headPos = transform.position;
+        var lookDir = transform.forward;
+
+        void DrawRay(Vector3 pts, Vector3 dir) => Handles.DrawAAPolyLine(pts, pts + dir);
+
+        if (Physics.Raycast(headPos, transform.forward, out RaycastHit hit))
+        {
+            Vector3 hitPos = hit.point;
+            Vector3 normal = hit.normal;
+
+            Handles.color = Color.green;
+            Handles.DrawAAPolyLine(headPos, hitPos);
+            Handles.color = Color.cyan;
+            DrawRay(hitPos, hit.normal);
+        }
+        else
+        {
+            Handles.color = Color.red;
+            DrawRay(headPos, lookDir);
+        }
+
+    }
+#endif
+}
--- Assets/Scripts/LocalToWorld.cs
+++ Assets/Scripts/LocalToWorld.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 /* 
@@ -22,9 +23,10 @@ You need to do this manually, using the dot product and vector math!
 */
 public class LocalToWorld : MonoBehaviour
 {
-    public Transform localObj;
-    public Transform WorldObj;
+    public Vector3 localObj;
+    public Vector3 WorldObj;
 
+    public Transform LocalToWorldPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -34,10 +36,54 @@ public class LocalToWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
-        {
-            //From local to world. get the origin of local and then see the world space in that. 
-        }   
+
     }
 
+    #if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        var objpos = transform.position;
+        var up =  transform.forward;
+        var right = transform.right;
+
+        DrawXZGizmos(objpos, up, right);
+        DrawXZGizmos(Vector3.zero, Vector3.forward, Vector3.right);
+
+        //var worldPos = TransformLocalToWorld(this.localObj);
+        //Gizmos.DrawSphere(worldPos, 0.1f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(this.WorldObj, 0.1f);
+        var localPos = TransformWorldToLocal(this.WorldObj);
+        this.LocalToWorldPoint.localPosition = localPos;
+    }
+    #endif
+
+    //take in a world position and transform it to a local position
+    //1. Get the relative offset between the world position and the local space origin in world space
+    //2. With the difference between those 2 point, we can use it to realign it with the normal of the LOCAL Axis
+    //3. We use the DotProduct to project the difference to the local object space x,z axis  
+    //4. Returned result is the localPosition Vector
+    Vector3 TransformWorldToLocal(Vector3 worldPosition) {
+        var relativeOffset = worldPosition - this.transform.position;
+        var z = Vector3.Dot(relativeOffset, this.transform.forward);
+        var x = Vector3.Dot(relativeOffset, this.transform.right);
+        return new Vector3(x, 0, z);
+    }
+
+    //take in a local position and transform it to a world position
+    //1. Get the forward and right of the object space, multiple/scale it by the local space length
+    Vector3 TransformLocalToWorld(Vector3 localPosition) {
+        var z = transform.forward * localPosition.z;
+        var x = transform.right * localPosition.x;
+        return z + x + transform.position;
+    }
+
+    private void DrawXZGizmos(Vector3 point, Vector3 up, Vector3 right)
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(point, up);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(point, right);
+        Gizmos.color = Color.white;
+    }
 }
